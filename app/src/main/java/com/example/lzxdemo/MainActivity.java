2fa74e1e711f52490747c59bfa2c1c82ae2ee441
lzx <liuzhenxin@agridata.org>
2021-12-15 08:57:28 +0800

    commit

--- app/src/main/java/com/example/lzxdemo/MainActivity.java
+++ app/src/main/java/com/example/lzxdemo/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.lzxdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.lzxdemo.immersionbar.ImmersionBar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
       Button test_btn = findViewById(R.id.test_btn);
         test_btn.setOnClickListener(v -> {
-
+            SimpleDateFormat   formatter   =  new   SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date curDate =  new Date(System.currentTimeMillis());
+            formatter.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+            String format = formatter.format(curDate);
+            Log.d("lzx---->"," now time" +format );
         });
     }
 }
